9903accf083ac9f07f38248015117907aaf11351
Jaquiel Paim <jaquiel.paim@gmail.com>
2022-03-11 22:23:09 +0000

    Solved issue about deprecated options o mongoose

--- config/db.js
+++ config/db.js
@@ -2,21 +2,13 @@ const mongoose = require('mongoose')
 
 const { config } = require('./index')
 
-// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
-// by default, you need to set it to false.
-mongoose.set('useFindAndModify', false);
-
 const mongoUri = config.DB.ConnectionString
 
 const InitiateMongoServer = async () => {
     try {
 
         await mongoose.connect(
-            mongoUri, {
-                useCreateIndex: true,
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            })
+            mongoUri, {})
         console.log('DB connected!')
 
     } catch(e) {
