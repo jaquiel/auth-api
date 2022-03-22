d453110d585cdf64a655df00ed532b93965a269a
Jaquiel Paim <jaquiel.paim@gmail.com>
2022-03-22 15:37:41 +0000

    Added CORS to app

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ app.use(express.json())
 app.set('json spaces', 4)
 
 /**
+ * 
+ */
+const cors = require('cors')
+app.use(
+    cors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    }))
+
+/**
  * Routes
  */
 app.use('/', indexRoute)
